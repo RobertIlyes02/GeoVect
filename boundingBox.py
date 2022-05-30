3d1ce10ee350c0f5459f87672c4739ccb751556d
RobertIlyes02 <66189148+RobertIlyes02@users.noreply.github.com>
2022-05-29 20:19:10 -0400

    Update boundingBox.py

--- boundingBox.py
+++ boundingBox.py
@@ -1,13 +1,13 @@
 import json
 
 def boundingBox(vector):
-    vector['vector']['coordinates'] = json.loads(vector['vector']['coordinates'])
-    print(vector['vector']['coordinates'])
+    vector['coordinates'] = json.loads(vector['coordinates'])
+    print(vector['coordinates'])
     minx = None
     miny = None 
     maxx = None
     maxy = None
-    for idx, point in enumerate(vector['vector']['coordinates']):
+    for idx, point in enumerate(vector['coordinates']):
         if idx == 0:
             minx = point[0]
             maxx = point[0]
@@ -24,10 +24,3 @@ def boundingBox(vector):
             maxy = point[1]
     return {"type": "polygon", "coordinates": [[minx, miny], [maxx, miny], [maxx, maxy], [minx, maxy], [minx, miny]]}
             
-            
-print(boundingBox({'type': 'vector', 'vector': {'geometry': 'polygon', 'coordinates': '[[0, 0], [1,0], [1, 1], [0, 1], [0, 0]]'}}))
-print(boundingBox({'type': 'vector', 'vector': {'geometry': 'Polygon', 'coordinates': '[[0, 0], [1,2], [2, 0], [0, 0]]'}}))
-print(boundingBox({'type': 'vector', 'vector': {'geometry': 'line', 'coordinates': '[[0, 10], [20, 50]]'}}))
-print(boundingBox({'type': 'vector', 'vector': {'geometry': 'line', 'coordinates': '[[0, 0], [1, 1], [2,2], [3,3], [4,4]]'}}))
-print(boundingBox({'type': 'vector', 'vector': {'geometry': 'point', 'coordinates': '[[10, 10]]'}}))
-print(boundingBox({'type': 'vector', 'vector': {'geometry': 'Line', 'coordinates': '[[-0.342, 1.9234], [12.04, -50.123], [2, 1]]'}}))
